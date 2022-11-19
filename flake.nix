6ab3196a4fad79f7378ce62e1c24d0d6b321e23a
Mr. Fluffy <git@sixearsm>
2022-11-19 11:07:10 +0000

    fstat->1.0.2.24

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
     base1.url        = github:sixears/base1/r0.0.9.31;
-    parsec-plus.url  = github:sixears/parsec-plus/r1.1.1.37;
+    parsec-plus.url  = github:sixears/parsec-plus/r1.1.1.38;
     parser-plus.url  = github:sixears/parser-plus/r1.0.7.25;
     textual-plus.url = github:sixears/textual-plus/r1.0.2.25;
   };
