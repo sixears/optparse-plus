42b3d3ae30ea80ff1c9b548e331324baad27b4bc
Mr. Fluffy <git@sixearsm>
2022-11-19 06:47:29 +0000

    base0t->0.0.1.13

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
-    base1.url        = github:sixears/base1/r0.0.9.29;
-    parsec-plus.url  = github:sixears/parsec-plus/r1.1.1.27;
-    parser-plus.url  = github:sixears/parser-plus/r1.0.7.21;
-    textual-plus.url = github:sixears/textual-plus/r1.0.2.23;
+    base1.url        = github:sixears/base1/r0.0.9.30;
+    parsec-plus.url  = github:sixears/parsec-plus/r1.1.1.28;
+    parser-plus.url  = github:sixears/parser-plus/r1.0.7.22;
+    textual-plus.url = github:sixears/textual-plus/r1.0.2.24;
   };
 
   outputs = { self, nixpkgs, build-utils
